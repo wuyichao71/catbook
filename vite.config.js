f5d8111108b2c58637856ff653ea59c84f257092
hapo <DZ1722034@smail.nju.edu.cn>
2025-06-23 03:31:14 +0900

    add command `npm run netlify` and generate client/netlify

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,14 @@ export default defineConfig(({ mode }) => {
   const root = path.resolve(__dirname, "client");
   const env = loadEnv(mode, root);
   const base = env.VITE_BASENAME;
+  const outdir = env.VITE_OUTDIR;
+  console.log(outdir);
+
   return {
     plugins: [react(), svgr()],
     root: path.resolve(__dirname, "client"), // Set the root directory for Vite
     build: {
-      outDir: path.resolve(__dirname, "client/dist"), // Output directory for production build
+      outDir: path.resolve(__dirname, `client/${outdir}`), // Output directory for production build
     },
     base: base,
     server: {
