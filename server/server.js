b911e642bdbf7b2c5a54bf3a0e7b84c84b065364
Yichao Wu <wuyichao71@126.com>
2025-06-26 14:57:45 +0900

    try deploy on render.com

--- server/server.js
+++ server/server.js
@@ -43,18 +43,20 @@ const allowedOrigins = [
   "https://catbook-api.onrender.com",
 ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
+
+app.use(cors());
 
 app.use(
   session({
